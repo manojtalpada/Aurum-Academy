b440c72357c0bf42b2cae9d03bd8cadbb79c8ef8
nayan <nayan.m@redsparkinfo.co.in>
2020-04-02 12:42:35 +0530

    add editor in cards module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,11 @@ import { LessonComponent } from './view/lesson/lesson.component';
 import { PaypalDemoComponent } from './view/paypal-demo/paypal-demo.component';
 import { NgxPayPalModule } from 'ngx-paypal';
 import { DeckFilterPipe } from './view/deck/datafilterpipe';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+import { AngularMarkdownEditorModule } from 'src/lib/angular-markdown-editor';
 
 const APP_CONTAINERS = [
   FullLayoutComponent,
@@ -96,9 +101,32 @@ export function provideConfig() {
     ReactiveFormsModule,
     TabsModule,
     NgxPayPalModule,
+    AngularEditorModule,
     ButtonsModule.forRoot(),
     ModalModule.forRoot(),
     AlertModule.forRoot(),
+    CKEditorModule,
+
+    MarkdownModule.forRoot({
+      provide: MarkedOptions,
+      useFactory: (): MarkedOptions => {
+        return {
+          renderer: new MarkedRenderer(),
+          gfm: true,
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false,
+        };
+      },
+    }),
+    AngularMarkdownEditorModule.forRoot({
+      // add any Global Options/Config you might want
+      // to avoid passing the same options over and over in each components of your App
+      iconlibrary: 'glyph'
+    })
     
   ],
   providers: [
--- src/app/view/deck/deck.component.ts
+++ src/app/view/deck/deck.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AunumService } from 'src/app/services/aunumServices';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+ import { MarkdownService } from 'ngx-markdown';
+import { EditorInstance, EditorOption } from 'src/lib/angular-markdown-editor';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-deck',
@@ -9,6 +13,17 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./deck.component.css']
 })
 export class DeckComponent implements OnInit {
+  htmlContent = '';
+  bsEditorInstance: EditorInstance;
+  name = 'Angular 6'; 
+  templateForm: FormGroup;
+  editorOptions: EditorOption;
+  form: FormGroup;
+  markdownText: string;
+  showEditor = true;
+  public Editor = ClassicEditor
+
+
   public deckList;
   deck : any = {};
   dataFilter;
@@ -16,13 +31,168 @@ export class DeckComponent implements OnInit {
   public filterQuery = "";
   public addcards = false;
   userid = sessionStorage.getItem('userid');
-  constructor(private fb: FormBuilder,private aunumservices: AunumService,private _route: ActivatedRoute) {
+  constructor(private fb: FormBuilder,private fb1 :FormBuilder,private aunumservices: AunumService,private _route: ActivatedRoute, private markdownService: MarkdownService) {
     this.getAllDeck();
+    this.buildForm(this.markdownText);
+
+    this.form = fb1.group({
+      // name: ['John'],
+      // surname: ['Doe'],
+      description: []
+    });
+
    }
 
   ngOnInit() {
+    this.editorOptions = {
+      autofocus: false,
+      iconlibrary: 'fa',
+      savable: false,
+      onShow: (e) => this.bsEditorInstance = e,
+      parser: (val) => this.parse(val)
+    };
+  }
+// config: AngularEditorConfig = {
+//     editable: true,
+//     spellcheck: true,
+//     height: '15rem',
+//     minHeight: '5rem',
+//     placeholder: 'Enter text here...',
+//     translate: 'no',
+//     defaultParagraphSeparator: 'p',
+//     defaultFontName: 'Arial',
+//     toolbarHiddenButtons: [
+//       ['bold']
+//       ],
+//     customClasses: [
+//       {
+//         name: "quote",
+//         class: "quote",
+//       },
+//       {
+//         name: 'redText',
+//         class: 'redText'
+//       },
+//       {
+//         name: "titleText",
+//         class: "titleText",
+//         tag: "h1",
+//       },
+//     ],
+    
+//   };
+
+public config = {
+  // fontFamily requires a plugin to be built into the editor
+  
+  fontFamily: {
+    options: [
+      'default',
+      'Arial, Helvetica, sans-serif',
+      'Courier New, Courier, monospace',
+      'Georgia, serif',
+      'Lucida Sans Unicode, Lucida Grande, sans-serif',
+      'Tahoma, Geneva, sans-serif',
+      'Times New Roman, Times, serif',
+      'Trebuchet MS, Helvetica, sans-serif',
+      'Verdana, Geneva, sans-serif'
+    ]
+  },
+  // fontSize requires a plugin to be built into the editor
+  fontSize: {
+    options: [
+      9,
+      11,
+      12,
+      13,
+      'default',
+      17,
+      19,
+      21
+    ]
+  },
+  // table options not necessary if we keep default config
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells'
+    ]
+  },
+  // toolbar options not necessary if we keep default config
+  toolbar: [
+    'undo',
+    'redo',
+    '|',
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'blockQuote',
+    'link', 
+    
+    '|',
+     'bulletedList',
+    'numberedList',
+    '|',
+    'insertTable',
+    'mediaEmbed',
+    'MathType',
+    'ChemType'
+    
+    
+  ],
+};
+  buildForm(markdownText) {
+    this.templateForm = this.fb.group({
+      body: [markdownText],
+      isPreview: [true]
+    });
+  }
+  parse(inputValue: string) {
+    const markedOutput = this.markdownService.compile(inputValue.trim());
+    this.highlight();
+
+    return markedOutput;
+  }
+  
+  highlight() {
+    setTimeout(() => {
+      this.markdownService.highlight();
+    });
   }
 
+  hidePreview() {
+    if (this.bsEditorInstance && this.bsEditorInstance.hidePreview) {
+      this.bsEditorInstance.hidePreview();
+    }
+  }
+
+  showFullScreen(isFullScreen: boolean) {
+    if (this.bsEditorInstance && this.bsEditorInstance.setFullscreen) {
+      this.bsEditorInstance.showPreview();
+      this.bsEditorInstance.setFullscreen(isFullScreen);
+    }
+  }
+  onFormChanges(): void {
+    this.templateForm.valueChanges.subscribe(formData => {
+      if (formData) {
+        this.markdownText = formData.body;
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log('Form submit:', this.form.value);
+  }
+
+  reset() {
+    this.form.reset();
+  }
+
+  get description() {
+    return this.form.get('description');
+  }
 
   openDeckModel(data){
     this.deck = data;
